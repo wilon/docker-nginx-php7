5008e88ffb43dff59815ef0ca90e373481e08238
weilong <wilonx@163.com>
2017-09-22 18:01:06 +0800

    update ldap ext script

--- storage/ldap/setup.sh
+++ storage/ldap/setup.sh
@@ -3,6 +3,6 @@
 THIS_DIR="$(cd `dirname $0`; pwd)"
 WORKSPACE="$(cd `dirname $0`/../..; pwd)"
 
-cp -rf $THIS_DIR/ldap-7.1.0.so $WORKSPACE/phpext_file/
-cp -rf $THIS_DIR/ldap-7.1.0.ini $WORKSPACE/phpext_ini/
-cp -rf $THIS_DIR/extension_ssl.sh $WORKSPACE/phpext_file/
+cp -rf $THIS_DIR/ldap-7.1.0.so $WORKSPACE/phpext_file/ldap.so
+cp -rf $THIS_DIR/ldap-7.1.0.ini $WORKSPACE/phpext_ini/ldap.ini
+cp -rf $THIS_DIR/extension_ssl.sh $WORKSPACE/phpext_file/extension.sh
